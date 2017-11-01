8d739e7440abd7453df47a9037aa686165880f56
Eddy To <d33pra1n@gmail.com>
2017-11-01 07:33:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  cell = "   "
+  wall = "|"
+  row = "-----------"
+
+  board = "#{cell}#{wall}#{cell}#{wall}#{cell}\n
+           #{cell}#{wall}#{cell}#{wall}#{cell}\n
+           #{cell}#{wall}#{cell}#{wall}#{cell}\n"
+           
+puts(board)
+
+end
